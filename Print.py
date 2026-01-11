1df77f0c7455428812162acb85c0cd42917d2342
Alex5564 <fallguysknockout43@gmail.com>
2026-01-11 18:21:51 +0100

    Add files via upload

---
+++ Print.py
@@ -0,0 +1,17 @@
+print("hello")
+      
+#this is a statement
+print("hi welcome to python code")
+
+#this is a number
+print(4)
+
+#this is a line
+print("hello /n")
+
+#this is a number and a digit
+print("hello" ,4)
+
+
+print("hello" ,end="*")
+
