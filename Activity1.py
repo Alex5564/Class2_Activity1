48831371017e344b6a066896c036ee4fa2d68036
Alex5564 <fallguysknockout43@gmail.com>
2026-01-11 18:19:51 +0100

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,15 @@
+a=5 
+print("type of a:", type(a))
+
+b=1.0
+print("type of b:", type(b))
+
+g= "Hello"
+print("type of g:", type(g))
+
+h= True 
+print("type of h:", type(h))
+
+
+b=Name
+print("type of b:")
