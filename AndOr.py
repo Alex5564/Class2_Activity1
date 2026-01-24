92d634b224ad7d584cb2c3ad39d8456771b89c4d
Alex5564 <fallguysknockout43@gmail.com>
2026-01-24 18:04:05 +0100

    Add files via upload

---
+++ AndOr.py
@@ -0,0 +1,15 @@
+a=10
+b=12
+c=0
+
+if a and b and c: 
+ print("all values are true")
+else:
+ print("at least one value is false")
+
+# This is a OR operator
+
+if a or b or c:
+ print("at least one value is true")
+else:
+ print("all values are false")
