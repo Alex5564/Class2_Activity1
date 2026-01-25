039f18896c0a30fdce5f44c5445cc5a0a8592d1e
Alex5564 <fallguysknockout43@gmail.com>
2026-01-25 18:20:41 +0100

    Add files via upload

---
+++ BitwiseOperator.py
@@ -0,0 +1,15 @@
+a=5
+b=-10
+
+#this is a right shift operator
+
+print("a >> 1=", a >> 1)
+print("a >> 1=", b >> 1)
+
+a=5
+b=-10
+
+#this is a left shift operator
+
+print("a << 1=", a << 1)
+print("a << 1=", b << 1)
---
+++ IdentityOperator.py
@@ -0,0 +1,16 @@
+a=10
+if(type(a)is int):
+ print("True")
+
+else:
+ print("False")
+
+b=5.5
+if(type(b)is not float):
+ print("True")
+
+else:
+ print("False")
+
+
+ 
