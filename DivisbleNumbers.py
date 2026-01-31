d363cd3de720351c4f01abdcc6e342207d568c3f
Alex5564 <fallguysknockout43@gmail.com>
2026-01-31 18:13:58 +0100

    Add files via upload

---
+++ DivisbleNumbers.py
@@ -0,0 +1,12 @@
+print("Enter a Number (Nominator):")
+numn=int(input())
+print("Enter a Number (Denominator):")
+numd=int(input())
+
+if numn%numd==0:
+ print(str (numn)+ " is divisble by "+ str (numd))
+
+else:
+ print(str (numn)+ " is not divisble by "+ str (numd))
+
+
---
+++ OperatorPrecedence.py
@@ -0,0 +1,20 @@
+v=4
+w=3
+x=8
+y=2
+z=0
+
+z=(v+w)*x/y
+print(z)
+
+name="John" 
+age=0
+
+if name=="John" or name== "Alex" and age>=2:
+ print("Hello Welcome")
+
+else:
+ print("Goodbye")
+
+
+
