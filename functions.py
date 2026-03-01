7d48f5d71c53f22c3ca5d43dd66119b4e0e7dc67
Alex5564 <fallguysknockout43@gmail.com>
2026-03-01 18:12:44 +0100

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,14 @@
+def well_wishes():
+  print("hello")
+  print("how are you?")
+well_wishes()
+
+def weather():
+   autumn = "cool"
+   spring = "warm"
+   print("the weater is", autumn)
+   print("the weather is", spring)
+weather()
+  
+
+  
