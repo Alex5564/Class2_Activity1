9a7cdb343cccecfa2dcdc7c558358d70b52cac5a
Alex5564 <fallguysknockout43@gmail.com>
2026-03-22 18:14:52 +0100

    Add files via upload

---
+++ TrogonometricValue.py
@@ -0,0 +1,12 @@
+import math 
+
+angle_degree = 45
+angle_radians = math.radians(angle_degree)
+
+sine_value = math.sin(angle_radians)
+cosine_value = math.cos(angle_radians)
+tangent_value = math.tan(angle_radians)
+
+print(f"Sine of {angle_degree} degrees: {sine_value}")
+print(f"Cosine of {angle_degree} degrees: {cosine_value}")
+print(f"Tangent of {angle_degree} degrees: {tangent_value}")
