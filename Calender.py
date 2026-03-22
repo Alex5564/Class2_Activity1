ae9d968b4885546d7ac7dbddab93806bf537a34d
Alex5564 <fallguysknockout43@gmail.com>
2026-03-22 18:09:10 +0100

    Add files via upload

---
+++ Calender.py
@@ -0,0 +1,7 @@
+import calendar
+
+print(calendar.calendar(2026))
+
+print(calendar.month(2026, 1))
+
+print(calendar.weekday(2024, 12, 27))
---
+++ DateTime.py
@@ -0,0 +1,10 @@
+import datetime
+
+print(datetime.datetime.now())
+
+today = datetime.date.today()
+print(today)
+
+d = datetime.date(2024, 12, 27)
+print(d)
+
