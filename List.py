f52bd7895108716d7cf013e0789cf7dd906fcc98
Alex5564 <fallguysknockout43@gmail.com>
2026-03-28 18:00:32 +0100

    Add files via upload

---
+++ List.py
@@ -0,0 +1,13 @@
+empty_list = []
+print(empty_list)
+
+
+list_of_numbers = [1, 2, 3, 4, 5]
+print(list_of_numbers)
+
+triple = [1, 2, 3] * 3
+print(triple)
+
+alist = [1, 2, 3, 4, 5]
+alist = alist[::-1]
+print(alist, "\n")
