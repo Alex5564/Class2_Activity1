e07a1b3c87a02b7d61eb2a758175d17d561d3f85
Alex5564 <fallguysknockout43@gmail.com>
2026-03-29 19:04:41 +0200

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,11 @@
+tuplex = ("tuplex", 3, 4.3, False)
+print(tuplex)        
+
+tuplex = (4, 6, 2, 8, 3, 9, 1, 5, 7)
+print(tuplex)
+
+tuplex = (tuplex[3:5])
+print(tuplex)
+
+tuplex = tuplex + (9,)
+print(tuplex)
