63a001e3b66cc1e129553db6c6558fd9fc10d925
Alex5564 <fallguysknockout43@gmail.com>
2026-04-11 19:05:01 +0200

    Add files via upload

---
+++ Set.py
@@ -0,0 +1,11 @@
+my_set = (1, 2, 3, 4, 5)
+print(my_set)
+
+my_set = {1.0, "Hello", (1, 2, 3, 4, 5)}
+print(my_set)
+
+my_set = {1, 2, 3, 4, 3, 2, 1}
+print(my_set)
+
+my_set = set([(1, 2, 3, 2, 3)])
+print(my_set, "\n")
