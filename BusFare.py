0418a49fe214a8d52d91969595bc0b25e9f56f0e
Alex5564 <fallguysknockout43@gmail.com>
2026-05-03 18:07:52 +0200

    Add files via upload

---
+++ BusFare.py
@@ -0,0 +1,17 @@
+class vehicle: 
+    def __init__(self, name, seating_capacity):
+        self.name = name
+        self.seating_capacity = seating_capacity
+
+    def fare(self):
+        return self.seating_capacity * 100
+         
+
+class Bus(vehicle):
+    def fare(self):
+        base_fare = super().fare()
+        total_fare = base_fare + (0.1 * base_fare) 
+        return total_fare 
+
+school_bus = Bus("School Bus", 50)
+print(f"Total Bus Fare: INR {school_bus.fare()}")
