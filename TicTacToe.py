cf211fb069fd25de8b5e8065e5b331178d8283d8
Alex5564 <fallguysknockout43@gmail.com>
2026-07-18 19:11:34 +0200

    Add files via upload

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,42 +1,47 @@
-board = ["", "", ""
-        , "", "", ""
-        , "", "", ""]
+import random
+import colorama import init, Fore, Style
+init(autoreset=True)
 
-def print_board():
-    print(board[0] + " | " + board[1] + " | " + board[2])
-    print("---------")
-    print(board[3] + " | " + board[4] + " | " + board[5])
-    print("---------")
-    print(board[6] + " | " + board[7] + " | " + board[8])
+def display_board(board):
+    print()
+    def coloured_cell(cell):
+        if cell == "X":
+            return Fore.RED + cell + Style.RESET_ALL
+        elif cell == "O":
+            return Fore.BLUE + cell + Style.RESET_ALL
+        else:
+            return Fore.YELLOW + cell + Style.RESET_ALL
+        
+    print(' ' + coloured_cell(board[0]) + ' / ' + coloured_cell(board[1]) + ' / ' + coloured_cell(board[2]))
+    print(Fore.CYAN + '---+---+---' + Style.RESET_ALL)
+    print(' ' + coloured_cell(board[3]) + ' / ' + coloured_cell(board[4]) + ' / ' + coloured_cell(board[5]))
+    print(Fore.CYAN + '---+---+---' + Style.RESET_ALL)
+    print(' ' + coloured_cell(board[6]) + ' / ' + coloured_cell(board[7]) + ' / ' + coloured_cell(board[8]))
+    print()
 
-def check_win(player):
-    win_positions = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], # rows
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], # columns
-    [0, 4, 8], [2, 4, 6]             # diagonals
-    ]
-
-    for pos in win_positions:
-        if board[pos [0]] == board[pos[1]] == board[pos[2]] == player:
-            return True
-    return False
-
-Player = "X"
-for turn in range(9):
-    print_board()
-    move = int(input(f"PLayer {Player}, enter your move (0-8): "))
-    if board[move] == "":
-        board[move] = Player
+def player_choice(board):
+    symbol = ''
+    while symbol not in ['X', 'O']:
+        symbol = input("Choose your symbol (X or O): ").upper()
+    if symbol == 'X':
+        return 'X', 'O'
     else:
-        print(" Position already taken!")
-        continue
+        return 'O', 'X'
+    
+def player_move(board, symbol):
+    move = -1
+    while move not in range (1, 10) or not board[move - 1].isdigit():
+        try:
+            move = int(input(f"Player {symbol}, enter your move (1-9: "))
+            if move not in range(1, 10) or not board[move - 1].isdigit():
+                print("Invalid move. Please try again.")
+        except ValueError:
+            print("Please enter a value between 1 and 9.")
+    board[move - 1] = symbol
 
-    if check_win(Player):
-        print_board()
-        print(f"Player {Player} wins!")
-        break
+    
 
-    Player = "O" if Player == "X" else "X"
-else:
-    print_board()
-    print("It's a draw!")
+
+
+    
+
