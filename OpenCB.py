50ef74607e4daff5a793826ab728663517f6ae31
Alex5564 <fallguysknockout43@gmail.com>
2026-08-02 19:04:43 +0200

    Add files via upload

--- OpenCB.py
+++ OpenCB.py
@@ -3,6 +3,9 @@ import cv2
 image = cv2.imread('spark.png')
 cv2.imshow("spark", image)
 
+gray=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("gray", gray)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
