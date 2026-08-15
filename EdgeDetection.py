91bd886de70eea4aaf2ee407db8a3070733d25f1
Alex5564 <fallguysknockout43@gmail.com>
2026-08-15 19:35:38 +0200

    Add files via upload

--- EdgeDetection.py
+++ EdgeDetection.py
@@ -39,11 +39,39 @@ def interactive_edge_detection(image_path):
            display_image("Sobel Edge Detection", combined_sobel)
 
         elif choice == '2':
-            print(" Adjust thresholds for Canny Edge Detection:")
-            
+            print(" Adjust thresholds for Canny ( default: 100 and 200 )")
+            lower_thresh = int(input("Enter lower threshold: "))
+            upper_thresh = int(input("Enter upper threshold: "))
+            edges = cv2.Canny(gray_image, lower_thresh, upper_thresh)
+            display_image("Canny Edge Detection", edges)
 
+        elif choice == '3':
+           laplacian = cv2.Laplacian(gray_image, cv2.CV_64F)
+           display_image("Laplacian Edge Detection", np.abs(laplacian).astype(np.uint8))
 
-
+        elif choice == '4':
+           print("Adjust kernel size for Gaussian blur (must be odd, default: 5)")
+           kernel_size = int(input("Enter kernel size (odd number): "))
+           blurred = cv2.GaussianBlur(image, (kernel_size, kernel_size), 0)
+           display_image("Gaussian Smoothed Image", blurred)
+           
+        elif choice == "5":
+           print("Adjust kernel size for Median filtering (must be odd, default: 5)")
+           kernel_size = int(input("Enter kernel size (odd number): "))
+           median_filtered = cv2.medianBlur(image, kernel_size)
+           display_image("Median Filtered Image", median_filtered)
+           
+        elif choice == "6":
+          print("Exiting...")
+          break
+           
+        else:
+           print("Invalid choice. Please select a number between 1 and 6.")
+           
+interactive_edge_detection('example.jpg')
+           
+           
+           
 
    
 
