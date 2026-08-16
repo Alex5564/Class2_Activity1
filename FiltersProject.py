8242d6d5a4922f2e80566dd592f2e8fbabbb4710
Alex5564 <fallguysknockout43@gmail.com>
2026-08-16 19:22:22 +0200

    Add files via upload

---
+++ FiltersProject.py
@@ -0,0 +1,68 @@
+import cv2
+import numpy as np
+
+def apply_color_filter(image, filter_type):
+
+    filtered_image = image.copy()
+
+    if filter_type == "red":
+        filtered_image[:, :, 1] = 0
+        filtered_image[:, :, 0] = 0
+    elif filter_type == "blue_tint":
+
+        filtered_image[:, :, 1] = 0
+        filtered_image[:, :, 2] = 0
+
+    elif filter_type == "green_tint":
+        filtered_image[:, :, 0] = 0
+        filtered_image[:, :, 2] = 0 
+
+    elif filter_type == "increase_red":
+
+        filtered_image[:, :, 2] = cv2.add(filtered_image[:, :, 2], 50)
+
+    elif filter_type == "decrease_Orange":
+
+        filtered_image[:, :, 0] = cv2.subtract(filtered_image[:, :, 0], 50)
+    return filtered_image
+
+image_path = 'app_img.jpg'
+image = cv2.imread(image_path)
+
+if image is None:
+    print("Error: Image not found.")
+else:
+    filter_type = "original"
+
+    print('Press the followung keys to apply filters:')
+    print('r - Red Tint')
+    print('b - Blue Tint ')
+    print('g - Green Tint')
+    print('c - Cyan Tint')
+    print('v - Violet Tint')
+    print('o - Orange Tint')
+    print('w - White Tint')
+    print('a - Increase Green Intensity')
+    print('s - Decrease Orange Intensity')
+
+    while True:
+
+        filtered_image = apply_color_filter(image, filter_type)
+
+        cv2.imshow('Filtered Image', filtered_image)
+
+        key = cv2.waitKey(0) & 0xFF
+
+        if key == ord('r'):
+            filter_type = "red_tint"
+        elif key == ord('b'):
+            filter_type = "blue_tint"
+        elif key == ord('g'):
+            filter_type = "green_tint"
+      
+        elif key == ord('q'):
+            print("Exiting...")
+            break
+        else:
+            print("Invalid key! Please use 'r', 'b', 'g', 'i', 'd' or 'q'.")
+cv2.destroyAllWindows()
