09623f3cf6bf33373918141f1ec6ea8d0b8454b3
Alex5564 <fallguysknockout43@gmail.com>
2026-09-05 19:07:10 +0200

    Add files via upload

---
+++ Brightness&Volume.py
@@ -0,0 +1,25 @@
+import math
+
+
+length = math.hypot(
+    x2 - x1,
+    y2 - y1
+)
+
+if length < 50:
+    print("Low Volume")
+if length > 200:
+    print("High Volume")
+
+if distance < 50:
+    brightness = 20
+if distance < 100:
+    brightness = 50
+
+else:
+    brightness = 100
+
+    
+    
+
+
